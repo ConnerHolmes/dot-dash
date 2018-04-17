d6dd8f7f9a56eb618b166cec8c00c5fdd7ebf984
Conner Holmes <conner.a.holmes@gmail.com>
2018-04-16 22:16:24 -0500

    Somewhat working condition.
    Translation works.
    Working on polish

---
+++ js/app.js
@@ -0,0 +1,82 @@
+// Morse code alphabet
+const alphabet = [
+{letter: 'a', seq: '. _'},
+{letter: 'b', seq: '_ . . .'},
+{letter: 'c', seq: '_ . _ .'},
+{letter: 'd', seq: '_ . .'},
+{letter: 'e', seq: '.'},
+{letter: 'f', seq: '. . _ .'},
+{letter: 'g', seq: '_ _ .'},
+{letter: 'h', seq: '. . . .'},
+{letter: 'i', seq: '. .'},
+{letter: 'j', seq: '. _ _ _'},
+{letter: 'k', seq: '_ . _'},
+{letter: 'l', seq: '. _ . .'},
+{letter: 'm', seq: '_ _'},
+{letter: 'n', seq: '_ .'},
+{letter: 'o', seq: '_ _ _'},
+{letter: 'p', seq: '. _ _ .'},
+{letter: 'q', seq: '_ _ . _'},
+{letter: 'r', seq: '. _ .'},
+{letter: 's', seq: '. . .'},
+{letter: 't', seq: '_'},
+{letter: 'u', seq: '. . _'},
+{letter: 'v', seq: '. . . _'},
+{letter: 'w', seq: '. _ _'},
+{letter: 'x', seq: '_ . . _'},
+{letter: 'y', seq: '_ . _ _'},
+{letter: 'z', seq: '_ _ . .'}
+];
+
+const inputWord = document.getElementById('inputWord').querySelectorAll('p');
+const outputSeq = document.getElementById('outputSeq');
+
+function populateWord(word){
+  inputWord[0].textContent = word;
+};
+
+// Display given sequence
+function populateSeq(seq){
+  // Create node
+  let para = document.createElement('p');
+  // Populate node
+  let child = document.createTextNode(seq);
+  // Append node
+  para.appendChild(child);
+  outputSeq.appendChild(para);
+};
+
+
+function input(word){
+  for(letter in word){
+    matchChar(word[letter]);
+  }
+};
+
+
+// Find the character in the alphabet
+function matchChar(char){
+  let y = alphabet.length - 1;
+  while(y >= 0){
+    if(char === alphabet[y].letter){
+      // Pass character sequence
+      populateSeq(alphabet[y].seq)
+      y = -1;
+    } else {
+      y -= 1;
+    }
+}
+};
+
+let output = document.getElementById("transButton").addEventListener("click", function( event ) {
+    let inputVal = document.getElementById('input').value;
+    let button = document.getElementById('transButton');
+    let buttonVal = button.value.toLowerCase()
+    buttonVal = inputVal;
+    let splitInput = buttonVal.split("");
+    // Clear field
+    outputSeq.innerHTML = '';
+
+    populateWord(buttonVal);
+    input(splitInput);
+  }, false);
